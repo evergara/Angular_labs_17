6118d7364e66a885d922d796e8fee2202426010f
Emerson vergara <emerson.vergara>
2023-11-11 05:30:27 -0500

    Commit - preparacion del proyectos base

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LabsComponent } from './pages/labs/labs.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'labs',
+    component: LabsComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  }
+];
---
+++ src/app/pages/labs/labs.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-labs',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './labs.component.html',
+  styleUrl: './labs.component.scss'
+})
+export class LabsComponent {
+
+}
